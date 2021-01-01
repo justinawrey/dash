02adf4359ccc1dd16e6e08a83420b222a8966036
Justin Awrey <awreyjustin@gmail.com>
2021-01-01 15:52:05 -0800

    stub blogs

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,46 +1,13 @@
 <script>
+  import Router from "svelte-spa-router";
   import Background from "./Background.svelte";
-  import Button from "./Button.svelte";
-  import ThemePicker from "./ThemePicker.svelte";
   import TrackMouse from "./TrackMouse.svelte";
+  import ThemePicker from "./ThemePicker.svelte";
+
+  import routes from "../routes";
 </script>
 
-<div class="vertical-centered">
-  <h1 class="header">Justin Awrey</h1>
-  <h2 class="subheader">One stop web designer + developer.</h2>
-  <Button size="sm">Take the tour 🕵️</Button>
-</div>
-
+<Router {routes} />
 <Background />
 <TrackMouse />
 <ThemePicker />
-
-<style>
-  .vertical-centered {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .vertical-centered :global(button) {
-    background-color: transparent;
-  }
-
-  .header,
-  .subheader {
-    margin: 0;
-    color: var(--light);
-    transition: color 500ms;
-  }
-
-  .header {
-    font-size: var(--fs--xl);
-  }
-
-  .subheader {
-    font-size: var(--fs--md);
-    margin-top: var(--spacing2);
-    margin-bottom: var(--spacing24);
-  }
-</style>
