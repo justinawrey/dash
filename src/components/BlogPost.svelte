85fe249967f422c276b09c8eb11a2d4a1530d746
Justin Awrey <awreyjustin@gmail.com>
2021-01-03 10:44:01 -0800

    add social links

--- src/components/BlogPost.svelte
+++ src/components/BlogPost.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { pageFade } from "../routes/routeUtil";
   import posts from "../posts";
 
   export let params = {};
@@ -6,7 +7,7 @@
   const { title, subtitle, time, dateCreated } = $posts[id];
 </script>
 
-<div class="post">
+<div class="post" in:pageFade={{ delay: 500 }} out:pageFade>
   <h1>{title}</h1>
   <h2>{subtitle}</h2>
   <span>{time}</span>
--- src/components/TopRight.svelte
+++ src/components/TopRight.svelte
@@ -4,6 +4,36 @@
   import LinkedIn from "../svg/LinkedIn.svelte";
 </script>
 
-<ThemePicker />
-<Octocat />
-<LinkedIn />
+<div class="top-right">
+  <ThemePicker />
+  <a
+    href="https://www.linkedin.com/in/justin-awrey-2b91a7119/"
+    target="_blank"
+    style="margin-bottom: 10px"
+  >
+    <LinkedIn />
+  </a>
+  <a href="https://github.com/justinawrey" target="_blank">
+    <Octocat />
+  </a>
+</div>
+
+<style>
+  .top-right {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    right: 0;
+    fill: var(--light);
+  }
+
+  a {
+    margin-left: 25px;
+  }
+
+  a :global(svg) {
+    fill: var(--light);
+  }
+</style>
